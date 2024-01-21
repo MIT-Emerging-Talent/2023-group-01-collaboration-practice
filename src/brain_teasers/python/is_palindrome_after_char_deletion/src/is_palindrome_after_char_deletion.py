de6eddb5e1d993f61f2fa752e018ca4ffca7a70d
SujoodEldouma <sigoodk@gmail.com>
2024-01-21 16:42:33 +0200

    Solution for is palindrome

---
+++ src/brain_teasers/python/is_palindrome_after_char_deletion/src/is_palindrome_after_char_deletion.py
@@ -0,0 +1,8 @@
+def is_palindrome_after_char_deletion(word):
+    ''' 
+    This function checks wether the string is palindrome or not
+    args:
+    word (string) : the string to be checked out
+    '''
+    print(word[::-1])
+    return word == word[::-1]
